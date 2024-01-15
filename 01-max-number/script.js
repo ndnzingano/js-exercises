173226ec942dff6f6b0c0352885721751e5f2015
Nadine Zingano <nadine.zingano@ifood.com.br>
2024-01-15 11:29:33 -0300

    first exercises

---
+++ 01-max-number/script.js
@@ -0,0 +1,16 @@
+console.log('01-max-number');
+
+// Escreva uma função que retorne o maior número entre dois números
+
+function findMaxNumber(num1, num2){
+
+}
+
+let checkMax1 = findMaxNumber(10, 5);
+console.log('Max Number:',checkMax1);
+
+let checkMax2 = findMaxNumber(10, 15);
+console.log('Max Number:',checkMax2);
+
+let checkMax3 = findMaxNumber(100, 100);
+console.log('Max Number:',checkMax3);
---
+++ 02-fizzbuzz-algorithms/script.js
@@ -0,0 +1,32 @@
+console.log('02-fizzbuzz-algorithms');
+
+
+// Escreva uma função que resolva o problema do FizzBuzz.
+// Se o parâmetro de entrada for divisível por 3, imprima Fizz;
+// Se o parâmetro de entrada for divisível por 5, imprima Buzz;
+// Se o parâmetro de entrada for divisível por 3 e 5, imprima FizzBuzz;
+// Se o parâmetro de entrada não for um número, imprima 'Nan - Not a Number! Please Input Number';
+// Se o parâmetro de entrada for outro número, imprima o número.
+
+
+function isfizzBuzz(arg){
+
+}
+
+let checkFizzBuzz1 = isfizzBuzz('one');
+console.log(checkFizzBuzz1);
+
+let checkFizzBuzz2 = isfizzBuzz(true);
+console.log(checkFizzBuzz2);
+
+let checkFizzBuzz3 = isfizzBuzz(9);
+console.log(checkFizzBuzz3);
+
+let checkFizzBuzz4 = isfizzBuzz(10);
+console.log(checkFizzBuzz4);
+
+let checkFizzBuzz5 = isfizzBuzz(30);
+console.log(checkFizzBuzz5);
+
+let checkFizzBuzz6 = isfizzBuzz(11);
+console.log(checkFizzBuzz6);
